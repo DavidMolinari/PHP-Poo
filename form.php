d97813a20ca965b502021fb9b82d3d803a6c2d47
David Molinari <Davvmoli@gmail.com>
2016-04-23 17:48:21 +0200

    meh

--- form.php
+++ form.php
@@ -1,26 +1,54 @@
 <?php
+/**
+ * Class Form
+ * Permet de générer un petit formulaire.
+ */
 class Form{
 
+  /**
+   * @var array Données utilisées par le form
+   */
   private $data;
+
+  /**
+   * @var string tag pour entourer les champs
+   */
   public $surround = 'p';
-  // Ctor
+
+
+
+  /**
+   * @param array $data
+   */
   public function __construct($data = array()){
     $this->data = $data;
   }
 
-
+  /**
+   * @param $html code html à retourner
+   * @return string
+   */
   private function surround($html){
 
     return "<{$this->surround}>{$html}</{$this->surround}>";
   }
 
+
+  /**
+   * $index string
+   * @return string
+   */
+  
   private function getValue($index) {
+
     return isset($this->data[$index]) ? $this->data[$index] : null;
-    
+
   }
 
-
-  // Fonction inPut un type Text
+  /**
+   * $name string
+   * @return string
+   */
   public function input($name){
       return $this->surround('<input type="text" name="' . $name . '"value="'
       . $this->getValue($name) .
@@ -28,13 +56,10 @@ class Form{
 }
 
 
-  // Fonction Button Submit
-
+  /**
+   * @return string
+   */
   public function submit(){
     return $this->surround('<button type="submit">Envoyer</button>');
 }
-
-
 }
-
- ?>
--- index.php
+++ index.php
@@ -1,23 +1,19 @@
 <?php
+
 require 'form.php';
 
-$form = new Form(array('username' => 'Roger'));
-
-
-echo $form->input('username');
-echo $form->input('password');
-echo $form->submit();
-
-
-
-$form = new Form();
-
-
-echo $form->input("Meh");
-echo $form->input("Meh");
-echo $form->input("Meh");
-echo $form->input("Meh");
-echo $form->input("Meh");
-echo $form->submit();
-
+$form = new Form($_POST)
  ?>
+<form action="#" method="post">
+  <?php
+
+
+
+  echo $form->input('username');
+  echo $form->input('password');
+  echo $form->submit();
+
+  $form->submit();
+
+   ?>
+</form>
