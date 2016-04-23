25f93d37be2da1419829373a5bad84aef0b83c4b
David Molinari <Davvmoli@gmail.com>
2016-04-23 19:05:05 +0200

    form btn heritage

--- BootstrapForm.php
+++ BootstrapForm.php
@@ -2,16 +2,34 @@
 
 class BootstrapForm extends Form{
 
-  protected function surround($html){
+
+  /**
+   * @param $html code html à retourner
+   * @return string
+   */
+  protected function surround($html)
+  {
 
     return "<div class=\"form-group\">{$html}</div>";
   }
 
+  /**
+   * $name string
+   * @return string
+   */
   public function input($name){
       return $this->surround(
       '<label>' . $name . '</label>' .
       '<input type="text" name="' . $name . '"value="'
       . $this->getValue($name) . '" class="form-control"' .
       '">');
+}
+
+
+/**
+ * @return string
+ */
+public function submit(){
+  return '<button type="submit" class="btn btn-primary">Envoyer</button>';
 }
 }
